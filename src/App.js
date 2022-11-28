e74ba3f043c8516829027a0c6c305868664d12fa
DESKTOP-4RQUT1S\PC-13 <dnjstjsthd@gmail.com>
2022-11-28 12:32:55 +0900

    modified:   package-lock.json 	modified:   package.json 	new file:   public/img/jp0.png 	new file:   public/img/jp1.png 	new file:   public/img/jp2.png 	new file:   public/img/jp3.png 	new file:   public/img/tree.png 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Items.jsx 	modified:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Container, Navbar, Nav} from 'react-bootstrap';
+import {useState} from 'react';
+// import jp1 from './img/jp1.png';
+// import jp2 from './img/jp2.png';
+// import jp3 from './img/jp3.png';
+// import jp4 from './img/jp4.png';
+// import jp5 from './img/jp5.png';
+import {Items} from './Items';
+import $ from 'jquery';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  let arr = [1,2,3,4,5,6,7,8];
+  let [modal, setModal] = useState(0);
+  let mstyle ={
+    position: 'absolute', width:'50px',
+    height: '50px', top: '10px', right:'20px',
+    background: 'red', color: 'white',
+    borderRadius: '8px',
+    fontSize: '20pt'
+  };
+  return(
+    <div className='App'>
+      {/* 메뉴바 */}
+      <Navbar bg='light' expand='lg'>
+        <Container>
+          <Navbar.Brand href='#home'>ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link href='#home'>Home</Nav.Link>
+              <Nav.Link href='#cart'>Cart</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/*  메인이미지 */}
+      <div className='main-bg'></div>
+      <div className='container'>
+        <div className='row'>
+          {
+            arr.map((v,i) => {
+              if(v>3) v=0;
+              return( <Items key={i} ord={v}/>)
+            })
+          }
+        </div>
+      </div>
+      <div className='modBox' style={{display: 'none'}}>
+        <div style={{width: '100%', position: 'relative'}}>
+          <div style={mstyle} onClick={() => {$('.modBox').hide();}}>X</div>
+        </div><br />
+      <div>
+        <h3>aa</h3>
+        <p></p>
+      </div>
+      </div>
+    {/* <Button variant='primary'>primary 버튼 </Button> */}
     </div>
   );
 }
---
+++ src/Items.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import {useState} from 'react';
+import $ from 'jquery';
+
+
+export const Items = (props) => {
+    let[likecnt, likecntchk] = useState(0); //0 은  false와 같음
+    let divStyle = {backgroundImage: 'url('+process.env.PUBLIC_URL + '/img/jp'+props.ord+'.png)'};
+
+    return(
+        <div className='col-lg-4' onClick={(e) => {
+            let x = e.pageX;
+            const y = e.pageY;
+            console.log(`x축: ${x}, y축: ${y}`);
+            $('.modBox').show();
+            $('.modBox').css({top: y+'px', left: x+'px'});
+            $('.modBox h3').text('상품 '+props.ord);
+            $('.modBox p').text('상품 '+props.ord+' 설명');
+        }}>
+            <div className='imgBox'>
+                <div className='imgCon' alt={"이미지"+props.ord} style={divStyle}/>
+                <h4>상품{props.ord}</h4>
+                <p>상품{props.ord} 설명</p>
+                <p className='lkcnt' onClick={() => likecntchk(++likecnt)}>좋아요 {likecnt}</p>
+            </div>
+
+        </div>
+    );
+}
